88b7a10f49299c7c44c61476cf68894467ff96f2
Marcelo Abbadia <marcelo.abbadia@gmail.com>
2025-05-28 23:31:16 +0100

    Created the components, pages, and configured Bootstrap, Font Awesome,some routes and fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'loja-hm';

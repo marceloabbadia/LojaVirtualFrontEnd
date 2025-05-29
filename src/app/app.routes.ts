b2a96b17e96983c3136aaa842646920b387b9857
Marcelo Abbadia <marcelo.abbadia@gmail.com>
2025-05-29 23:36:15 +0100

    Header completed, footer created, and Not Found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
   },
+  {
+    path: 'products',
+    component: Products,
+  },
+  { path: '**', component: NotFound },
 ];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Products } from '../../pages/products/products';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class Header {
-
+  auth = true;
 }

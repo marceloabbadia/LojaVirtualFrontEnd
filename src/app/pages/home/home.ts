827676a830d7e99d4d2b60f69bc1b5ed3f971a16
Marcelo Abbadia <marcelo.abbadia@gmail.com>
2025-06-01 01:25:58 +0100

    Create product page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Products } from "../products/products";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Products],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css'],
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+  currentIndex = 0;
+  animate = false;
 
+  images = [
+    'assets/images/slider1.jpg',
+    'assets/images/slider2.jpg',
+    'assets/images/slider3.jpg',
+  ];
+
+  private intervalId!: any;
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => this.nextSlide(), 3000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  private nextSlide(): void {
+    this.animate = true;
+
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+
+    setTimeout(() => (this.animate = false), 1000);
+  }
 }
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Product {
+  _id: string;
+  name: string;
+  price: number;
+  mainPhoto: string;
+}
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrl: './products.css',
 })
-export class Products {
+export class Products implements OnInit {
+  products: Product[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Product[]>('http://localhost:3000/product')
+      .subscribe((data) => (this.products = data.slice(0, 8)));
+  }
+
+  imageUrl(p: Product): string {
+    return `http://localhost:3000/images/${p.mainPhoto}`;
+  }
 }
